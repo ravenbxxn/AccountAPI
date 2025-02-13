2b03dce88f1a04846b2065f122ea847e01d45587
ravenbxxn <kw.waiwingrob@gmail.com>
2025-02-13 14:52:44 +0700

    Delete WebPrototype directory

--- WebPrototype/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebPrototype.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
